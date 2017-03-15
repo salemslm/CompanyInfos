d839470cccb0ac1440f5ca161ff56dde7b711cf2
Salem <salem.b.mabrouk@gmail.com>
2017-03-15 20:32:08 +0100

    Web parsing Done. Read CSV done.

--- Js/Model.js
+++ Js/Model.js
@@ -2,11 +2,31 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var cheerioTableparser = require('cheerio-tableparser');
+var csv = require("fast-csv");
+
 var app = express();
 
 
+
+
+csv
+.fromPath("companies.csv")
+.on("data", function(arrayCompanies){
+  console.log(arrayCompanies);
+
+
+})
+.on("end", function(){
+  console.log("done");
+
+
+
 // J'ai cette information :
-siret =	53179916100025;
+siret =		50235330300014;
+// 66204244937404; BNP Paribas
+
+var codeNAF, companyName, address, postalCode, city, pays, natureEtablissement;
 
 /*
 // ou  ca :
@@ -29,15 +49,17 @@ request(url, function (error, response, body) {
     $('#identite_deno').filter(function(){
       var data = $(this);
       companyName = data.text();
-
-
     })
+
     companyName = cleanName(companyName);
     console.log("Nom retrouvé : " + companyName);
     //Then we see the desired esablissement.
     urlEtablissement = "http://www.societe.com/etablissements/" + companyName + "-" + siret.toString().substring(0, siret.toString().length - 5) + ".html";
     console.log("Redirection vers " + urlEtablissement + " ...");
     request(urlEtablissement, function (error, response, body) {
+      var $ = cheerio.load(body)
+
+
       //console.log('body:', body); // Print the HTML
       console.log("Redirection effectuée.");
       $('#etabs').filter(function(){
@@ -47,33 +69,88 @@ request(url, function (error, response, body) {
         var i = 1;
         //While the siret is'nt found
         while (data.children().children().first().next().next().next().children().first().next().text() != siret) {
-            data = data.next();
-            //console.log(i);
-            console.log(data.children().children().first().next().next().next().children().first().next().text());
-            //i++;
+          data = data.next();
+          //console.log(i);
+
+          console.log(data.children().children().first().next().next().next().children().first().next().text());
+          //i++;
 
         }
-        siretFound = data.children().children().first().next().next().next().children().first().next().text();
-
-        console.log(siretFound);
-
-        //Maintenant que nous avons retrouvé l'entreprise, nous allons sur l'url de cette entreprise et allons scraper tout ca.
-
+        urlEnd = data.children().children().first().next().next().next().children().first().next().children().first().attr('href');
+      });
+      //var datatest = $("#etab2").find("tbody > tr:nth-child(1) > td:nth-child(2) > a");
+      //console.log(datatest.text());
 
 
-      })
+      urlSiret = "http://www.societe.com" + urlEnd;
+      //Maintenant que nous avons retrouvé l'entreprise, nous allons sur l'url de cette entreprise et allons scraper tout ca.
+      console.log(urlSiret);
+      request(urlSiret, function (error, response, html) {
+
+        console.log("Redirection vers : " + urlSiret + "...");
+        if(!error){
+
+          var $ = cheerio.load(html)
+
+          cheerioTableparser($);
+
+          var table = $('#etab').parsetable();
+
+          address = findValue(table, "Adresse");
+          postalCode = findValue(table, "Code postal");
+          city = findValue(table, "Ville");
+          pays = findValue(table, "Pays");
+          codeNAF = findValue(table, "Code ape (NAF)");
+          natureEtablissement = findValue(table, "Nature de l&apos;&#xFFFD;tablissement");
+
+          console.log(address);
+          console.log(postalCode);
+          console.log(city);
+          console.log(pays);
+          console.log(codeNAF);
+          console.log(natureEtablissement);
 
 
+        }
+        else {
+          console.log("Erreur de connexion sur lien : " + urlSiret );
+        }
+
+
+        //console.log(dataa.text());
+
+
+//Ending Third callback
+      });
+
+
+//Ending Second callback
     });
 
 
   }
+  else{
+    console.log("Erreur de connexion" );
+  }
 
-
-
-
+//Ending first callback
 });
 
+//Ending CSV Parsing callback
+});
+
+
+function findValue(tablee, text){
+  var i = 0;
+  for(i = 0; i < tablee[0].length; i++){
+    //console.log(tablee[0][i] );
+    if(tablee[0][i] == text){
+      return tablee[1][i];
+    }
+
+  }
+  return "Unknown";
+}
 
 
 function cleanName(companyName){

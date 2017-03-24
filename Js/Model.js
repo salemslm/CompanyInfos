80a361cddc5521c2a9fdda81d68302adb13956fd
Salem <salem.b.mabrouk@gmail.com>
2017-03-24 23:48:38 +0100

    Ignore les cas longs et les erreurs de typo de SIRET

--- Js/Model.js
+++ Js/Model.js
@@ -4,14 +4,17 @@ var request = require('request');
 var cheerio = require('cheerio');
 var cheerioTableparser = require('cheerio-tableparser');
 var csv = require("fast-csv");
-
+var urlEnd;
 var app = express();
-
+var firmapi;
 var inputCompanies = [];
 var resultCompanies = [["SIRET", "Nom", "Addresse", "Code Postal", "Ville", "Pays", "code NAF", "Nature de l'etablissement"]];
+var codeNAF, companyName, address, postalCode, city, pays, natureEtablissement;
+var companyObject;
+
 
 csv
-.fromPath("companies.csv", {headers : true} )
+.fromPath("Companies_DPE.csv", {headers : true} )
 .on("data", function(arrayCompanies){
   inputCompanies.push(arrayCompanies);
 
@@ -21,30 +24,74 @@ csv
 
   //console.log(inputCompanies);
 
-  //console.log(inputCompanies);
+        uploader(1390);
 
-  // J'ai cette information :
 
-  // 66204244937404; BNP Paribas
+}); //Ending CSV Parsing callback
 
-  var codeNAF, companyName, address, postalCode, city, pays, natureEtablissement;
 
-  /*
-  // ou  ca :
-  // 80238479200015
-  // ou ca :
-  // 802384792
-  // ou ca :
-  // 334  3453 345   23
-  */
-  //get SIREN and not SIRET.
 
-  for(var i = 0 ; i<inputCompanies.length; i++){
 
-    siret  = inputCompanies[i].SIRET;
+function findValue(tablee, text){
+  //var i = 0;
+  for(i = 0; i < tablee[0].length; i++){
+    //console.log(tablee[0][i] );
+    if(tablee[0][i] == text){
+      return tablee[1][i];
+    }
+
+  }
+  return "Unknown";
+}
+
+
+function cleanName(companyName){
+  // _ espaces dans le nom d'entreprise
+  companyName=companyName.replace(/[ \u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g,'-')
+
+  //_ tirets apres le nom de l'entreprise à supprimer car inutile.
+  companyName = companyName.substring(0, companyName.length - 2);
+
+  // _ mettre en minuscule
+  companyName = companyName.toLowerCase();
+  return companyName;
+}
+
+
+
+
+
+function uploader(i) {
+
+  // //console.log(isTrueSIREN(inputCompanies[i].siret.toString().substring(0, inputCompanies[i].siret.toString().length - 5)));
+  // while(isTrueSIREN(inputCompanies[i].siret.toString().substring(0, inputCompanies[i].siret.toString().length - 5)) == false){
+  // //  console.log( isTrueSIREN(inputCompanies[i].siret.toString().substring(0, inputCompanies[i].siret.toString().length - 5)));
+  //   i++;
+  //
+  // }
+//  console.log(i);
+
+//Default features for companies
+address = "‘Unknown’"
+postalCode = "Unknown"
+city = "Unknown"
+pays = "Unknown"
+codeNAF = "Unknown"
+natureEtablissement = "Unknown"
+companyName = "Unknown"
+
+console.log(inputCompanies.length);
+
+if(i< inputCompanies.length){
+
+
+  //var isTrueSIREN = isTrueSIREN(inputCompanies[i].siret.toString().substring(0, siret.toString().length - 5));
+
+  siret  = inputCompanies[i].siret;
+  siren = siret.toString().substring(0, siret.toString().length - 5);
   url = 'http://www.societe.com/cgi-bin/fiche/?rncs='+ siret.toString().substring(0, siret.toString().length - 5);
   //console.log("Recherche du nom à partir du SIREN démarrée..");
-  //console.log("Redirection sur " + url + " ...");
+  console.log("Redirection sur " + url + " ...");
   request(url, function (error, response, body) {
     if(!error){
       //console.log("Redirection effectuée.");
@@ -55,13 +102,13 @@ csv
         companyName = data.text();
       })
 
+      console.log(siren);
+
       companyName = cleanName(companyName);
-      //console.log("Nom retrouvé : " + companyName);
 
       //Then we see the desired etablissement.
-      urlEtablissement = "http://www.societe.com/etablissements/" + companyName + "-" + siret.toString().substring(0, siret.toString().length - 5) + ".html";
-      //console.log("Redirection vers " + urlEtablissement + " ...");
-      console.log(urlEtablissement);
+      urlEtablissement = "http://www.societe.com/etablissements/" + companyName + "-" + siren + ".html";
+      console.log("Redirection vers " + urlEtablissement + " ...");
       request(urlEtablissement, function (error, response, body) {
         var $ = cheerio.load(body);
 
@@ -71,25 +118,38 @@ csv
 
           //First Siret
           data = data.children();
-          var i = 1;
+          var j = 1;
           //While the siret is'nt found
           while (data.children().children().first().next().next().next().children().first().next().text() != siret) {
             data = data.next();
+              j++;
+            //console.log(data.children().children().first().next().next().next().children().first().next().text());
+              if(j > 20){
+                //abandonment for this company
+                address = "‘Unknown’"
+                postalCode = "Unknown"
+                city = "Unknown"
+                pays = "Unknown"
+                codeNAF = "Unknown"
+                natureEtablissement = "Unknown"
+                companyName = "Unknown"
+                resultCompanies.push([siret, companyName, address, postalCode, city, pays, codeNAF, natureEtablissement]);
 
-            console.log(data.children().children().first().next().next().next().children().first().next().text());
+                return "Error";
+              }
 
           }
           urlEnd = data.children().children().first().next().next().next().children().first().next().children().first().attr('href');
-        });
-        //var datatest = $("#etab2").find("tbody > tr:nth-child(1) > td:nth-child(2) > a");
 
+        });
 
         urlSiret = "http://www.societe.com" + urlEnd;
+
         //Maintenant que nous avons retrouvé l'entreprise, nous allons sur l'url de cette entreprise et allons scraper tout ca.
         //console.log(urlSiret);
         request(urlSiret, function (error, response, html) {
 
-        //  console.log("Redirection vers : " + urlSiret + "...");
+          //  console.log("Redirection vers : " + urlSiret + "...");
           if(!error){
 
             var $ = cheerio.load(html)
@@ -114,10 +174,12 @@ csv
             // console.log(natureEtablissement);
 
             resultCompanies.push([siret, companyName, address, postalCode, city, pays, codeNAF, natureEtablissement]);
+            uploader(i+1);
             var ws = fs.createWriteStream("resultCompanies.csv");
             csv
             .write(resultCompanies, {headers: true})
             .pipe(ws);
+
 
             console.log(companyName +" (" + i + "/" + inputCompanies.length + ") scrapped.");
 
@@ -137,47 +199,15 @@ csv
       });
 
 
-    }
-    else{
-      console.log("Erreur de connexion" );
-    }
+     }
+     else{
+       console.log("Erreur de connexion" );
+     }
 
     //Ending first callback
   });
 
-//Fin du For
+  //Fin du For
 }
 
-
-
-
-}); //Ending CSV Parsing callback
-
-
-
-
-
-function findValue(tablee, text){
-  var i = 0;
-  for(i = 0; i < tablee[0].length; i++){
-    //console.log(tablee[0][i] );
-    if(tablee[0][i] == text){
-      return tablee[1][i];
-    }
-
-  }
-  return "Unknown";
-}
-
-
-function cleanName(companyName){
-  // _ espaces dans le nom d'entreprise
-  companyName=companyName.replace(/[ \u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g,'-')
-
-  //_ tirets apres le nom de l'entreprise à supprimer car inutile.
-  companyName = companyName.substring(0, companyName.length - 2);
-
-  // _ mettre en minuscule
-  companyName = companyName.toLowerCase();
-  return companyName;
 }
